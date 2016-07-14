68c658ed6e1ddc54a5621f6b164a45d544e2a0c7
Derek N <derek.newland@gmail.com>
2016-07-13 23:34:59 -0400

    simpleClass.py update

--- simpleClass.py
+++ simpleClass.py
@@ -14,8 +14,7 @@ def createServers(servers):
     print("Creating server with hostname " + str(server))
     server = Server(server)
 
-class Server(hostname):
-  hostname = None
+class Server():
   def __init__(self, hostname):
     self.hostname = hostname
     self.memory = random.randrange(512,65536,512)
@@ -27,12 +26,10 @@ serverList = genServerList()
 #print(serverList)
 #createServers(serverList)
 
-#for server in serverList:
-#  server = Server(server)
+for server in serverList:
+  server = Server(server)
 
-#print(server0.hostname)
-
-test = Server("testserver")
+print(server0.hostname)
 
 #for server in serverList:
 #  print("Hostname: " + server.hostname + '\t' + "Memory (MB): " + server.memory + '\t' + "Disk Space: " + server.disk)
